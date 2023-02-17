d65d5c58eb81c8d7a7c412fac847bdfbadd8b4e1
hulin <hulin@asiainfo.com>
2023-02-17 18:30:19 +0800

    增加emacs配置

---
+++ init.el
@@ -0,0 +1,18 @@
+
+;; 隐藏滚动条
+(scroll-bar-mode -1)
+
+;; 启用行号
+(global-linum-mode t)
+
+;; 隐藏工具栏
+(tool-bar-mode -1)
+
+;; 隐藏菜单栏
+;; (menu-bar-mode -1)
+
+;; 最大化窗口
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; 取消自动备份
+(setq make-backup-files nil)

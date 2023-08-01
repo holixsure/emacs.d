86d085bbdaa6cd86a855b549a5571758e6bd4b9e
HolixSure <holixsure@gmail.com>
2023-08-01 23:11:16 +0800

    add gruvbox-theme and smart-mode-line package

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -26,4 +26,14 @@
 
 (use-package restart-emacs)
 
+;; gruvbox-theme
+(use-package gruvbox-theme
+  :init (load-theme 'gruvbox-dark-soft t))
+
+(use-package smart-mode-line
+  :init
+  (setq sml/no-confirm-load-theme t)
+  (setq sml/theme 'respectful)
+  (sml/setup))
+
 (provide 'init-package)

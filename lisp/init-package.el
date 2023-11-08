cf49f45848f52ce65939e04ce6a3cc5fdcbd83fb
hulin <hulin@asiainfo.com>
2023-11-08 16:05:30 +0800

    add lua mode package

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -26,14 +26,17 @@
 
 (use-package restart-emacs)
 
-;; gruvbox-theme
-(use-package gruvbox-theme
-  :init (load-theme 'gruvbox-dark-soft t))
+(use-package lua-mode
+  :defer 1)
 
-(use-package smart-mode-line
-  :init
-  (setq sml/no-confirm-load-theme t)
-  (setq sml/theme 'respectful)
-  (sml/setup))
+;; gruvbox-theme
+;;(use-package gruvbox-theme
+;;  :init (load-theme 'gruvbox-dark-soft t))
+
+;;(use-package smart-mode-line
+;;  :init
+;;  (setq sml/no-confirm-load-theme t)
+;;  (setq sml/theme 'respectful)
+;;  (sml/setup))
 
 (provide 'init-package)

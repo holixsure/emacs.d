ab5dcb03a58475a79d661be729464a7030daa990
hulin <hulin@asiainfo.com>
2024-05-24 15:29:10 +0800

    edit emacs config

--- init.el
+++ init.el
@@ -6,4 +6,21 @@
 (require 'init-elpa)
 (require 'init-package)
 (require 'init-ui)
+(require 'init-org)
 
+(require 'sort-tab)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lua-mode use-package smart-mode-line restart-emacs gruvbox-theme counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -2,7 +2,8 @@
 (scroll-bar-mode -1)
 
 ;; 启用行号
-(global-linum-mode t)
+;; (global-linum-mode t)
+(global-display-line-numbers-mode 1)
 
 ;; 隐藏工具栏
 (tool-bar-mode -1)
